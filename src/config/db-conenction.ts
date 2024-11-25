d9b1805fa5f12e467217008c03e058bd6f5c49b4
Genaro Gonzalez <genarrogg@gmail.com>
2024-11-25 08:48:11 -0400

    Initial commit

---
+++ src/config/db-conenction.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import { log, path } from "functions";
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  /* storage: ":memory:", */
+  storage: path.join(__dirname, "/db.sqlite"),
+  logging: false,
+});
+
+sequelize
+  .sync()
+  .then(() => log.cyan("Base de datos y tablas creadas"))
+  .catch((error) => log.red("Error al sincronizar:", error));
+
+export default sequelize;
